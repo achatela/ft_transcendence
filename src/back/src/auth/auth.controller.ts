71fcc2e00c8ccf8f8c8c311a98329e0387b313c1
Alexis CHATELAIN <achatela@e2r9p8.clusters.42paris.fr>
2023-05-13 16:59:51 +0200

    removed useless comments in prisma schema

--- src/back/src/auth/auth.controller.ts
+++ src/back/src/auth/auth.controller.ts
@@ -37,7 +37,6 @@ export class AuthController {
         const payload = {username: userInput.username, id: user.id};
         const signature: string = this.jwtService.sign(payload, { secret: process.env.JWT_SECRET });
         await this.prismaService.user.update({ where: { username: userInput.username }, data: { JwtToken: signature } });
-        const tmp = await this.prismaService.user.findUnique({where: {username: userInput.username}});
         const request = await axios.get("https://api.intra.42.fr/v2/me", { headers: { Authorization: `Bearer ${personnal42Token.access_token}` } });
         const avatar = request.data.image.versions.small;
         await this.prismaService.user.update({ where: { username: userInput.username }, data: { avatar: avatar } });
