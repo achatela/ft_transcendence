e0310d45d57c03baa007ec4e3b4e604548aa4d75
Alexis CHATELAIN <achatela@e2r8p3.clusters.42paris.fr>
2023-05-18 16:07:27 +0200

    social page added

---
+++ src/front/src/Components/SocialPage.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+interface IProps {}
+
+interface IState {}
+
+export default class SocialPage extends Component<IProps, IState> {
+    constructor(props: IProps) {
+        super(props);
+        this.state = {};
+    }
+    
+    render(): JSX.Element {
+        return <div></div>;
+    }
+}
