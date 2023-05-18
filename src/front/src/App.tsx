01e2e5190728fae3229c77be8e43c073ecc94625
Alexis CHATELAIN <achatela@e2r8p3.clusters.42paris.fr>
2023-05-18 17:13:34 +0200

    some css for socialpage

--- src/front/src/App.tsx
+++ src/front/src/App.tsx
@@ -4,19 +4,19 @@ import HomePage from './Components/HomePage'
 import ModePage from './Components/ModePage'
 import ProfilePage from './Components/ProfilePage'
 import GameBoard from './Components/GameBoard'
+import SocialPage from './Components/SocialPage'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
         <Routes>
           <Route path="/" Component={HomePage} />
           <Route path="/mode" Component={ModePage} />
           <Route path="/profile" Component={ProfilePage} />
           <Route path="/game" Component={GameBoard} />
+          <Route path="/social" Component={SocialPage} />
         </Routes>
-      </div>
     </BrowserRouter>
   );
 }
--- src/front/src/Components/SocialPage.tsx
+++ src/front/src/Components/SocialPage.tsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import axios from "axios";
+import "./css/SocialPage.css";
 
 interface IProps {}
 
@@ -9,8 +11,34 @@ export default class SocialPage extends Component<IProps, IState> {
         super(props);
         this.state = {};
     }
-    
+
+    async componentDidMount(): Promise<void> {
+        const request = await axios.post('http://localhost:3333/auth/refresh_token', JSON.stringify({ refreshToken: sessionStorage.getItem('refreshToken'), login: sessionStorage.getItem('login') }), {headers: { 'Content-Type': 'application/json'}});
+        if (request.data.success == true) {
+            sessionStorage.setItem("accessToken", request.data.accessToken);
+            sessionStorage.setItem("refreshToken", request.data.refreshToken);
+        }
+        else
+            console.error(request.data.error);
+        setInterval(async () => {
+            const request = await axios.post('http://localhost:3333/auth/refresh_token', JSON.stringify({ refreshToken: sessionStorage.getItem('refreshToken'), login: sessionStorage.getItem('login') }), {headers: { 'Content-Type': 'application/json'}});
+                if (request.data.success == true) {
+                    sessionStorage.setItem("accessToken", request.data.accessToken);
+                    sessionStorage.setItem("refreshToken", request.data.refreshToken);
+                }
+                else
+                    console.error(request.data.error);
+            }
+        , 60000)
+    }
+
     render(): JSX.Element {
-        return <div></div>;
+        return (
+            <div>
+                <h1 className="title">social page</h1>
+                <div className="friend-list"></div>
+                <div className="chat"></div>
+            </div>
+        );
     }
 }
