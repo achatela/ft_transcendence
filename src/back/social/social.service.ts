7f7ee6ad01ba960f69b4b8570015d11e9b4877e3
achatela <achatela@student.42.fr>
2023-06-19 14:57:51 +0200

    fixed remove friend not working

--- src/back/social/social.service.ts
+++ src/back/social/social.service.ts
@@ -9,37 +9,46 @@ export class SocialService {
     constructor(private prismaService: PrismaService, private authService: AuthService) { }
 
     async removeFriend(removerUsername: string, removedUsername: string, refreshToken: string, accessToken: string): Promise<{ success: boolean, accessToken?: string, refreshToken?: string }> {
-        const remover = await this.prismaService.user.findUnique({ where: { username: removerUsername }, include: {friends: true} });
+        const remover = await this.prismaService.user.findUnique({ where: { username: removerUsername }, include: { friends: true } });
         const auth = await this.authService.checkToken(remover, refreshToken, accessToken);
         if (auth.success == false)
             return { success: false };
-        const removed = await this.prismaService.user.findUnique({ where: { username: removedUsername }, include: {friends: true} });
-        await this.prismaService.user.update({
-            where: { username: removerUsername },
-            data: {
-                friends: {
-                    delete: { id: removed.id }
-                },
-            },
-        });
-        await this.prismaService.user.update({
-            where: { username: removedUsername },
-            data: {
-                friends: {
-                    delete: { id: remover.id }
-                },
-            },
-        });
-          
+        const removed = await this.prismaService.user.findUnique({ where: { username: removedUsername }, include: { friends: true } });
+        const friend = await remover.friends.find((friend) => friend.friendId == removed.id);
+        const friend2 = await removed.friends.find((friend) => friend.friendId == remover.id);
+        console.log("friend:", friend);
+        console.log("friend2:", friend2);
+        if (friend == undefined || friend2 == undefined)
+            return { success: false };
+
+        await this.prismaService.friend.delete({ where: { id: friend.id } });
+        await this.prismaService.friend.delete({ where: { id: friend2.id } });
+        // await this.prismaService.user.update({
+        //     where: { username: removerUsername },
+        //     data: {
+        //         friends: {
+        //             delete: { id: removed.id }
+        //         },
+        //     },
+        // });
+        // await this.prismaService.user.update({
+        //     where: { username: removedUsername },
+        //     data: {
+        //         friends: {
+        //             delete: { id: remover.id }
+        //         },
+        //     },
+        // });
+
         return { success: true, accessToken: auth.accessToken, refreshToken: auth.refreshToken };
     }
 
     async acceptFriendRequest(accepterUsername: string, acceptedUsername: string, accessToken: string, refreshToken: string): Promise<{ success: boolean, accessToken?: string, refreshToken?: string }> {
-        const accepter = await this.prismaService.user.findUnique({ where: { username: accepterUsername }, include: {friends: true} });
+        const accepter = await this.prismaService.user.findUnique({ where: { username: accepterUsername }, include: { friends: true } });
         const auth = await this.authService.checkToken(accepter, refreshToken, accessToken);
         if (auth.success == false)
             return { success: false };
-        const accepted = await this.prismaService.user.findUnique({ where: { username: acceptedUsername }, include: {friends: true} });
+        const accepted = await this.prismaService.user.findUnique({ where: { username: acceptedUsername }, include: { friends: true } });
         const room = "friendChat(" + String(accepter.id) + "," + String(accepted.id) + ")";
         const chat = await this.prismaService.friendChat.create({
             data: {
@@ -49,13 +58,13 @@ export class SocialService {
         await this.prismaService.user.update({
             where: { username: accepterUsername },
             data: {
-              friends: {
-                create: {
-                    friendId: accepted.id,
-                    chatId: chat.id
-                }
-              },
-              friendRequests: {set: accepter.friendRequests.filter((requesterId) => requesterId !== accepted.id)}
+                friends: {
+                    create: {
+                        friendId: accepted.id,
+                        chatId: chat.id
+                    }
+                },
+                friendRequests: { set: accepter.friendRequests.filter((requesterId) => requesterId !== accepted.id) }
             },
         });
         await this.prismaService.user.update({
@@ -78,12 +87,12 @@ export class SocialService {
         if (auth.success == false)
             return { success: false };
         const declined = await this.prismaService.user.findUnique({ where: { username: declinedUsername }, select: { id: true } });
-        await this.prismaService.user.update({ where: { id: decliner.id }, data: { friendRequests: {set: decliner.friendRequests.filter((requesterId) => requesterId !== declined.id)} } });
+        await this.prismaService.user.update({ where: { id: decliner.id }, data: { friendRequests: { set: decliner.friendRequests.filter((requesterId) => requesterId !== declined.id) } } });
         return { success: true, accessToken: auth.accessToken, refreshToken: auth.refreshToken };
     }
 
     async sendFriendRequest(requesterUsername: string, requestedUsername: string, accessToken: string, refreshToken: string): Promise<{ error?: string, success: boolean, accessToken?: string, refreshToken?: string }> {
-        const requester = await this.prismaService.user.findUnique({ where: { username: requesterUsername }, include: {friends: true} });
+        const requester = await this.prismaService.user.findUnique({ where: { username: requesterUsername }, include: { friends: true } });
         const auth = await this.authService.checkToken(requester, refreshToken, accessToken);
         if (auth.success == false)
             return { error: "User not found.", success: false };
@@ -107,13 +116,13 @@ export class SocialService {
             return { success: false };
         const friendRequests = [];
         for (const requestId of user.friendRequests)
-            friendRequests.push((await this.prismaService.user.findUnique({ where: { id: requestId }, select: {username: true} })).username);
+            friendRequests.push((await this.prismaService.user.findUnique({ where: { id: requestId }, select: { username: true } })).username);
         console.log("friend requests", friendRequests);
         return { success: true, accessToken: auth.accessToken, refreshToken: auth.refreshToken, friendRequests: friendRequests };
     }
 
     async getFriends(username: string, accessToken: string, refreshToken: string): Promise<{ success: boolean, accessToken?: string, refreshToken?: string, friends?: string[] }> {
-        const user = await this.prismaService.user.findUnique({ where: { username: username } , include: { friends: true }});
+        const user = await this.prismaService.user.findUnique({ where: { username: username }, include: { friends: true } });
         const auth = await this.authService.checkToken(user, refreshToken, accessToken);
         if (auth.success == false)
             return { success: false };
@@ -123,12 +132,12 @@ export class SocialService {
         return { success: true, accessToken: auth.accessToken, refreshToken: auth.refreshToken, friends: friends };
     }
 
-    async getFriendChat(username: string, friendUsername: string, accessToken: string, refreshToken: string): Promise<{ success: boolean, accessToken?: string, refreshToken?: string, chat?: {room: string, messages: string[]} }> {
-        const user = await this.prismaService.user.findUnique({ where: { username: username }, include: { friends: true }});
+    async getFriendChat(username: string, friendUsername: string, accessToken: string, refreshToken: string): Promise<{ success: boolean, accessToken?: string, refreshToken?: string, chat?: { room: string, messages: string[] } }> {
+        const user = await this.prismaService.user.findUnique({ where: { username: username }, include: { friends: true } });
         const auth = await this.authService.checkToken(user, refreshToken, accessToken);
         if (auth.success == false)
             return { success: false };
-        const friendId = (await this.prismaService.user.findUnique({ where: { username: friendUsername }, select: { id: true }})).id;
+        const friendId = (await this.prismaService.user.findUnique({ where: { username: friendUsername }, select: { id: true } })).id;
         const messages = [];
         for (const friend of user.friends) {
             if (friend.friendId == friendId) {
@@ -136,7 +145,7 @@ export class SocialService {
                 for (const message of chat.messages)
                     messages.push(message.text)
                 console.log("messages", messages);
-                return { success: true, accessToken: auth.accessToken, refreshToken: auth.refreshToken, chat: {room: chat.room, messages: messages} };
+                return { success: true, accessToken: auth.accessToken, refreshToken: auth.refreshToken, chat: { room: chat.room, messages: messages } };
             }
         }
     }
