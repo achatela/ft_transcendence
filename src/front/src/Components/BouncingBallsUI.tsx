7e75cba5692c6ec52d5a1cfc2c5ae71653beca58
Hugo CARPENTIER <hcarpent@paul-f3Br4s4.clusters.42paris.fr>
2023-08-21 22:06:46 +0200

    responsive home page buttons + simplified bouncing balls implementation

---
+++ src/front/src/Components/BouncingBallsUI.tsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect, Dispatch, SetStateAction } from "react";
+import BouncingBall from './BouncingBall';
+
+
+const startingSpeed = 5;
+
+interface BouncingBallsUIProps {
+    queryType: number;
+}
+
+export default function BouncingBallsUI(props: BouncingBallsUIProps) {
+    const [speed, setSpeed] = useState(startingSpeed);
+    const [balls, setBalls] = useState([{ x: 900, y: 100 }]);
+
+    useEffect(() => {
+        const slider = document.getElementById("myRange");
+
+        slider!.addEventListener("input", () => {
+        // @ts-ignore: Object is possibly 'null'.
+        const newSpeed = slider.value;
+        setSpeed(newSpeed);
+        });
+
+        return () => {
+        slider!.removeEventListener("input", () => { });
+        };
+    }, [setSpeed]);
+
+    function addBall() {
+        setBalls((prevBalls) => [
+            ...prevBalls,
+            { x: Math.random() * window.innerWidth, y: Math.random() * window.innerHeight },
+        ]);
+    }
+    function removeBall() {
+        setBalls((prevBalls) => {
+            "accessToken"
+            if (prevBalls.length > 0) {
+              return prevBalls.slice(0, -1);
+            } else {
+              return prevBalls;
+            }
+          });
+    }
+
+    return (
+        <>
+        <div>
+            <button className="addBallButton" type="button" onClick={addBall}>Add Ball</button>
+            <button className="removeBallButton" type="button" onClick={removeBall}>Remove Ball</button>
+            <div className="slide-container">
+                <p className="speed">Speed: {speed}</p>
+                <input defaultValue={startingSpeed} type="range" min="0" max="10" className="slider" id="myRange" />
+            </div>
+        </div>
+        {balls.map((ball, index) => (
+            <BouncingBall key={index} speed={speed} queryType={props.queryType}/>
+        ))}
+        </>
+    );
+}
